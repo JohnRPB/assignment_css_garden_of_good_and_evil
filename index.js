b5cf012e37d7564e3c1570c3b15c494fa0a74c8a
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-20 13:05:55 -0500

    timer change

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 // INDEX.JS
-
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const hbs = expressHandlebars.create({
-  defaultLayout: 'main',
-  helpers: helpers.registered,
+  defaultLayout: 'main'
 });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(4200, () => {
   console.log('Hey.');
