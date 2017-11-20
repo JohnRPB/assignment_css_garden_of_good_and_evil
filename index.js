88980b71da4e3c29ea793d17d299f27bf57945b2
Johann <baptistajohann@gmail.com>
2017-11-20 12:55:18 -0800

    working on it

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const hbs = expressHandlebars.create({
   defaultLayout: 'main'
@@ -17,6 +19,12 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+  const preferences = {
+    const morals = req.body.morals;
+    const food = req.body.food;
+    const color = req.
+  };
+  preferences.
   res.render('index');
 });
 
